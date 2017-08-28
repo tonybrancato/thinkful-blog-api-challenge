a0a34d5d7d91127212b67fd7b7f1a331415633b1
Tony Brancato <tonybrancato1@gmail.com>
2017-08-28 18:04:56 -0400

    3/4 working. POST not working

--- test/test-server.js
+++ test/test-server.js
@@ -52,6 +52,44 @@ describe('BlogPosts', function() {
         res.body.should.be.a('object');
         res.body.should.include.keys('id', 'title', 'content', 'author', 'publishDate');
         res.body.id.should.not.be.null;
-        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id},));
       });
   });
+
+  it('should update blog posts on PUT', function() {
+
+    return chai.request(app)
+
+      .get('/blog-posts')
+      .then(function(res) {
+        const updatedPost = Object.assign(res.body[0], {
+          title: 'foo',
+          content: 'bar'
+        });
+        return chai.request(app)
+          .put(`/blog-posts/${res.body[0].id}`)
+          .send(updatedPost)
+          .then(function(res) {
+            res.should.have.status(204);
+          });
+      });
+  });
+
+  // test strategy:
+  //  1. GET a shopping list items so we can get ID of one
+  //  to delete.
+  //  2. DELETE an item and ensure we get back a status 204
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+      // first have to get so we have an `id` of item
+      // to delete
+      .get('/blog-posts')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blog-posts/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });  
+});
