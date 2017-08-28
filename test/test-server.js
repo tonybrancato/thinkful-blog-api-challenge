b190f354f291022db38a095492a883455b89f2eb
Tony Brancato <tonybrancato1@gmail.com>
2017-08-27 23:38:31 -0400

    rectified /test & test-server/js

--- test/test-server.js
+++ test/test-server.js
@@ -16,45 +16,24 @@ chai.use(chaiHttp);
 
 describe('BlogPosts', function() {
 
-  // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
-  // doing `return runServer`. If we didn't return a promise here,
-  // there's a possibility of a race condition where our tests start
-  // running before our server has started.
+
   before(function() {
     return runServer();
   });
 
-  // although we only have one test module at the moment, we'll
-  // close our server at the end of these tests. Otherwise,
-  // if we add another test module that also has a `before` block
-  // that starts our server, it will cause an error because the
-  // server would still be running from the previous tests.
   after(function() {
     return closeServer();
   });
 
-  // test strategy:
-  //   1. make request to `/shopping-list`
-  //   2. inspect response object and prove has right code and have
-  //   right keys in response object.
-  it('should list items on GET', function() {
-    // for Mocha tests, when we're dealing with asynchronous operations,
-    // we must either return a Promise object or else call a `done` callback
-    // at the end of the test. The `chai.request(server).get...` call is asynchronous
-    // and returns a Promise, so we just return it.
+  it('should list blog posts on GET', function() {
     return chai.request(app)
-      .get('/recipes')
+      .get('/blog-posts')
       .then(function(res) {
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
-
-        // because we create three items on app load
         res.body.length.should.be.at.least(1);
-        // each item should be an object with key/value pairs
-        // for `id`, `name` and `checked`.
-        const expectedKeys = ['id', 'name', 'ingredients'];
+        const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
         res.body.forEach(function(item) {
           item.should.be.a('object');
           item.should.include.keys(expectedKeys);
@@ -62,23 +41,17 @@ describe('BlogPosts', function() {
       });
   });
 
-  // test strategy:
-  //  1. make a POST request with data for a new item
-  //  2. inspect response object and prove it has right
-  //  status code and that the returned object has an `id`
   it('should add an item on POST', function() {
-    const newItem = {name: 'macchiato', ingredients: '2 oz espresso, 1 oz steamed whole mile, 5 oz foam of whole mile'};
+    const newItem = {title: 'How to Drink Coffee', content: 'sip it', author: 'a snarky beatnik'};
     return chai.request(app)
-      .post('/recipes')
+      .post('/blog-posts')
       .send(newItem)
       .then(function(res) {
         res.should.have.status(201);
         res.should.be.json;
         res.body.should.be.a('object');
-        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.should.include.keys('id', 'title', 'content', 'author', 'publishDate');
         res.body.id.should.not.be.null;
-        // response should be deep equal to `newItem` from above if we assign
-        // `id` to it from `res.body.id`
         res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
       });
   });
